19092eb071165a7da4cd45e40b30f745ece5c9be
Mehmet DEMİR <mmt.demr42@gmail.com>
2022-08-03 10:23:54 +0300

    Add project files.

---
+++ DataAccessLayer/Abstract/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface IRepository<TEntity>
+    {
+        List<TEntity> List();
+        void Insert(TEntity entity);
+        void Update(TEntity entity);
+        void Delete(TEntity entity);
+        List<TEntity> List(Expression<Func<TEntity, bool>> filter);
+    }
+}
---
+++ DataAccessLayer/Concrete/Repository/GenericRepository.cs
@@ -0,0 +1,45 @@
+﻿using DataAccessLayer.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete.Repository
+{
+    public class GenericRepository<T> : IRepository<T> where T : class
+    {
+        private readonly Context _context;
+        public GenericRepository(Context context)
+        {
+            _context = context;
+        }
+        public void Delete(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+            _context.SaveChanges();
+        }
+
+        public void Insert(T entity)
+        {
+            _context.Set<T>().Add(entity);
+            _context.SaveChanges();
+        }
+
+        public List<T> List()
+        {
+            return _context.Set<T>().ToList();
+        }
+
+        public List<T> List(Expression<Func<T, bool>> filter)
+        {
+            return _context.Set<T>().Where(filter).ToList();
+        }
+
+        public void Update(T entity)
+        {
+            _context.SaveChanges();
+        }
+    }
+}
