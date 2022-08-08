df12ff2e8c94ce434f9b3d25bdbd2757f67d47d0
Mehmet DEMİR <mmt.demr42@gmail.com>
2022-08-08 17:14:15 +0300

    Heading Index and AddHeading created.

--- MvcProjectKamp/Controllers/AdminHeadingController.cs
+++ MvcProjectKamp/Controllers/AdminHeadingController.cs
@@ -1,5 +1,7 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,10 +13,42 @@ namespace MvcProjectKamp.Controllers
     public class AdminHeadingController : Controller
     {
         HeadingManager manager = new HeadingManager(new EfHeadingDal());
+        CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+        WriterManager writerManager = new WriterManager(new EfWriterDal());
+        ValidationResult result;
         // GET: AdminHeading
         public ActionResult Index()
         {
             return View(manager.List());
+        }
+
+        public ActionResult AddHeading()
+        {
+            List<SelectListItem> valueCategory = (from category in categoryManager.List()
+                                                  select new SelectListItem
+                                                  {
+                                                      Text = category.CategoryName,
+                                                      Value = category.CategoryID.ToString(),
+                                                  }).ToList();
+
+            List<SelectListItem> valueWriter = (from writer in writerManager.List()
+                                                select new SelectListItem
+                                                {
+                                                    Text = writer.WriterName + " " + writer.WriterSurname,
+                                                    Value = writer.WriterID.ToString(),
+                                                }).ToList();
+            ViewBag.valueCategory = valueCategory;
+            ViewBag.valueWriter = valueWriter;
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult AddHeading(Heading heading)
+        {
+            heading.HeadingDate = DateTime.Parse(DateTime.Now.ToShortTimeString());
+            manager.Add(heading);
+            return RedirectToAction("Index");
         }
     }
 }
