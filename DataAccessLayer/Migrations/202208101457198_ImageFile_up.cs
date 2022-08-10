ff507adc5d88846363fc0ba7abef888d10ac5981
Mehmet DEMİR <mmt.demr42@gmail.com>
2022-08-10 19:49:41 +0300

    ImageFile created.

---
+++ DataAccessLayer/Migrations/202208101457198_ImageFile_up.cs
@@ -0,0 +1,18 @@
+﻿namespace DataAccessLayer.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ImageFile_up : DbMigration
+    {
+        public override void Up()
+        {
+            RenameTable(name: "dbo.ImageFıle", newName: "ImageFiles");
+        }
+        
+        public override void Down()
+        {
+            RenameTable(name: "dbo.ImageFiles", newName: "ImageFıle");
+        }
+    }
+}
---
+++ MvcProjectKamp/Controllers/ImageFileController.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjectKamp.Controllers
+{
+    public class ImageFileController : Controller
+    {
+        ImageFileManager manager = new ImageFileManager(new EfImageFileDal());
+        // GET: ImageFile
+        public ActionResult Index()
+        {
+            var images = manager.List();
+            return View(images);
+        }
+    }
+}
